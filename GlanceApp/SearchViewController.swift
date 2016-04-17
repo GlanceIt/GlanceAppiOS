c9f9ba5dd3d0917cf66130441b6f877a227c0778
Mamadi <mamadi@gmail.com>
2016-04-16 21:56:07 -0700

    Added 'Search Page' with one spot type ('Coffee Places')

--- GlanceApp/SearchViewController.swift
+++ GlanceApp/SearchViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var spotTypesTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.spotTypesTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
 
         // Do any additional setup after loading the view.
     }
@@ -26,5 +27,25 @@ class SearchViewController: UIViewController {
 //        // Get the new view controller using segue.destinationViewController.
 //        // Pass the selected object to the new view controller.
 //    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
     
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.spotTypesTable.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+
+        cell.textLabel?.text = "Coffee Places"
+        cell.textLabel?.numberOfLines = 0
+
+        return cell
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+        let searchResultsViewController = self.storyboard?.instantiateViewControllerWithIdentifier("searchResultsViewController") as! SearchResultsViewController
+        //searchResultsViewController.spotDetails = spotDetails
+        navigationController?.pushViewController(searchResultsViewController, animated: true)
+    }
+
 }
