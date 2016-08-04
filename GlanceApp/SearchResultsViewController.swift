30fc2275f391292c4c08f326f398a24e33c5a324
Mamadi <mamadi@gmail.com>
2016-08-03 18:45:20 -0700

    Changed page titles

--- GlanceApp/SearchResultsViewController.swift
+++ GlanceApp/SearchResultsViewController.swift
@@ -22,6 +22,7 @@ class SearchResultsViewController: UIViewController, UITableViewDelegate, UITabl
     override func viewDidLoad() {
         super.viewDidLoad()
         self.resultsTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.title = spotType
         
         getSpotList()
     }
